afcd7c2fcf0247151c24feca577bdb0b6005b5e9
Nokkvi <nos1@hi.is>
2016-10-14 20:21:48 +0000

    Skilgreiningar í processor.h

--- include/Processor.h
+++ include/Processor.h
@@ -12,6 +12,39 @@ class Processor
 public:
     Processor(Memory* pMemory);
     ~Processor();
+    NOP();
+    Increment(Register A);
+    Decrement(Register A);
+    Load(Register A, Register B);
+    Add(Register A, Register B);
+    Subtract(Register A, Register B);
+    Compare(Register A, Register B);
+    RotateLeft(Register A);
+    RotateRight(Register A);
+    ShiftLeft(Register A);
+    ShiftRight(Register A);
+    Complement(Register) A; //Logical NOT
+    OR(Register A, Register B);
+    AND(Register A, Register B);
+    XOR(Register A, Register B);
+    Push(Register SP);
+    Pop(Register SP);
+    ClearCarryFlag(Register F);
+    Reset(???);
 };
+
+private:
+    Register A;
+    Register B;
+    Register C;
+    Register D;
+    Register E;
+    Register H;
+    Register L;
+    Register F;
+    Register PC;
+    Register SP;
+    unsigned int mClock;
+    unsigned int tClock;
 
 #endif	/* PROCESSOR_H */
