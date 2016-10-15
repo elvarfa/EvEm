08062328c2cfdcd6beb6b898260e95a13c83888d
chirno <bsj9@hi.is>
2016-10-15 19:30:43 +0000

    Completed some opcodes and updated Processor.cpp

--- src/Processor.cpp
+++ src/Processor.cpp
@@ -16,3 +16,39 @@ void Processor::8Bit_Add(Register B){
     this->F = 0;
 
 }
+
+void Processor::LoadFrom(Register X){
+
+}
+
+void Processor::LoadFrom(){
+
+}
+
+void Processor::LoadTo(Register X){
+
+}
+
+void Processor::LoadTo(){
+
+}
+
+void Processor::DirectLoad(uint16_t n){
+
+}
+
+void Processor::DirectLoad(Register X, Register Y, uint16_t n){
+
+}
+
+void Processor::DirectLoadSP(uint16_t n){
+
+}
+
+void Processor::ImmediateLoad(Register X, uint8_t n){
+
+}
+
+void Processor::ImmediateLoad(Register X, Register Y, uint16_t n){
+
+}
